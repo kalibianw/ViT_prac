1de783fd116d95806e6ee235e3c34398b7e65d93
Jongbin Chae <kalibianw@gmail.com>
2022-08-24 01:08:00 +0900

    Remove model save line

--- main.py
+++ main.py
@@ -206,7 +206,6 @@ def run_experiment(model: models.Model):
     )
 
     model.load_weights(checkpoint_filepath)
-    model.save(filepath="model.h5")
     _, accuracy, top_5_accuracy = model.evaluate()
     print(f"Test accuracy: {round(accuracy * 100, 2)}%")
     print(f"Test top 5 accuracy: {round(top_5_accuracy * 100, 2)}%")
